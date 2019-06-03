bcd607f94ed9013407df6b607426085fdebf6679
rafaelrolivares <rafaelrolivares@gmail.com>
2019-06-03 14:34:05 +0200

    setup

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const socketIo = require('socket.io')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-
+const Sequelize = require('sequelize')
+const sequelize = require('./db')
 const app = express()
 app.use(cors())
 app.use(bodyParser.json())
@@ -15,40 +16,64 @@ function dispatchMessages () {
   io.emit('action', action)
 }
 
-app.post('/message', (request, response) => {
-  const { message } = request.body
-  // const message = request.body.message
-
-  console.log('message test:', message)
-
-  messages.push(message)
-
-  dispatchMessages()
-
-  response.status(201).send(message)
+app.post('/message', (req, res, next) => {
+  Message
+    .create(req.body)
+    .then(message => messages.push(message.message))
+    .then(dispatchMessages)
+    .then(message => {
+      return res.status(201).send(message)
+    })
+    .catch(error => {
+      console.log(error)
+      next(error)
+    })  
 })
+
+app.get('/message', (req, res, next) => { 
+  Message.findAll()
+  .then(message => {
+    res.send({ message: message })
+  })
+  .catch(error => next(error))
+}) 
 
 function onListen () {
   console.log('Listening on port 4000')
 }
 
 const server = app.listen(4000, onListen)
-// app.listen(4000, () => console.log("Listening..."))
-//
 const io = socketIo.listen(server)
-
-const messages = ['goodbye']
+const messages = []
 
 io.on(
   'connection',
   client => {
     console.log('client.id test:', client.id)
-
     dispatchMessages()
-
     client.on(
       'disconnect',
       () => console.log('disconnect test:', client.id)
     )
   }
 )
+
+const Message = sequelize.define(
+  'messages', {
+    message: {
+      type: Sequelize.TEXT,
+      allowNull: false
+    }
+  }
+)
+
+const User = sequelize.define(
+  'users', {
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false
+    }
+  }
+)
+
+Message.belongsTo(User)
